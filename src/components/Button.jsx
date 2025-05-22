d8a8edef7d415a73662b1ce51fd2b42fa2355768
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-22 22:13:25 +0530

    Enhance Hero section with GSAP animations and add AnimatedCounter component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,19 @@
 
 const Button = ({text, className, id}) => {
     return (
-        <a className={`${className ?? ''} cta-wrapper`}>
+        <a
+            onClick={(e) => {
+                e.preventDefault();          // Stop the link from jumping instantly
+                const target = document.getElementById(id);             // Find the section with ID "counter"
+
+                if(target && id){           // Only scroll if we found the section and an ID is passed in that prevents the contact button from scrolling to the top
+                    const offset = window.innerHeight * 0.15;           // Calculate how far down the page we need to scroll
+                    const top = target.getBoundingClientRect().top + window.pageYOffset - offset;
+                    window.scrollTo({top, behavior: "smooth"});         // Scroll smoothly to that position
+                }
+            }}
+        
+        className={`${className ?? ''} cta-wrapper`}>
             <div className="cta-button group">
                 <div className="bg-circle" />
                 <p className="text">{text}</p>
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,34 @@
 import { words } from '../constants/words.js';
 import Button from '../components/Button.jsx';
+import AnimatedCounter from '../components/AnimatedCounter.jsx';
 import HeroExperience from "../components/HeroModels/HeroExperience";
 
+import {useGSAP} from '@gsap/react';
+import gsap from 'gsap';
+
+import { TextPlugin } from "gsap/TextPlugin";
+gsap.registerPlugin(TextPlugin);
+
+
 const Hero = () => {
+    useGSAP( () => {
+        gsap.fromTo(".hero-text h1",
+            {y:50, opacity: 0},
+            {y:0, opacity:1, duration: 1, stagger: 0.2, ease: "power2.inOut"},
+        );
+
+        gsap.fromTo(
+            ".scrambled-para",
+            { text: "" },
+            {
+            duration: 3,
+            text: "Hi, I am Kartavya Shrivastav, a developer based in India with a passion for building impactful digital experiences. I specialize in crafting modern web applications that blend creativity with functionality.",
+            ease: "none"
+            }
+        );
+    });
+    
+
     return (
         <section id='hero' className="relative overflow-hidden" >
             <div className='absolute top-0 left-0 z-10'>
@@ -15,7 +41,7 @@ const Hero = () => {
                 <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
                     <div className='flex flex-col gap-7'>
                         <div className='hero-text'>
-                            <h1>Shaping 
+                            <h1>Shaping
                                 <span className='slide'>
                                     <span className='wrapper'>
                                         {words.map((word) => (
@@ -31,11 +57,11 @@ const Hero = () => {
                             <h1>that Deliver Results</h1>
                         </div>
 
-                        <p>
+                        <p className="scrambled-para text-white-50 md:text-xl md:w-xl pointer-events-none relative z-10">
                             Hi, I am Kartavya Shrivastav, a developer based in India with a passion for building impactful digital experiences. I specialize in crafting modern web applications that blend creativity with functionality.
                         </p>
 
-                        <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="See my Work" />
+                        <Button className="md:w-80 md:h-16 w-60 h-12" id="counter" text="See my Work" />
                     </div>
                 </header>
 
@@ -47,6 +73,10 @@ const Hero = () => {
                 </figure>
 
             </div>
+            
+            {/*Counter Section*/}
+            <AnimatedCounter />
+            
         </section>
     );
 };
