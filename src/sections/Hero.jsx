4466a829b85c935f6784a14816b824a5119416a6
Kartavya-Shrivastav <kartavyashrivastav28@gmail.com>
2025-05-22 20:33:27 +0530

    Add HeroExperience component to enhance the 3D model display in the Hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,6 @@
 import { words } from '../constants/words.js';
 import Button from '../components/Button.jsx';
+import HeroExperience from "../components/HeroModels/HeroExperience";
 
 const Hero = () => {
     return (
@@ -9,35 +10,43 @@ const Hero = () => {
             </div>
         
             <div className='hero-layout'>
-            {/*Left: Hero Content*/}
-            <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
-                <div className='flex flex-col gap-7'>
-                    <div className='hero-text'>
-                        <h1>Shaping 
-                            <span className='slide'>
-                                <span className='wrapper'>
-                                    {words.map((word) => (
-                                            <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
-                                                <img src={word.imgPath} alt={word.text} className='xl:size-12 md:size-10 size-7 md:pd-2 p-1 rounded-full bg-white-50'/>
-                                                <span>{word.text}</span>
-                                            </span>
-                                    ))}
+
+                {/*Left: Hero Content*/}
+                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                    <div className='flex flex-col gap-7'>
+                        <div className='hero-text'>
+                            <h1>Shaping 
+                                <span className='slide'>
+                                    <span className='wrapper'>
+                                        {words.map((word) => (
+                                                <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                                    <img src={word.imgPath} alt={word.text} className='xl:size-12 md:size-10 size-7 md:pd-2 p-1 rounded-full bg-white-50'/>
+                                                    <span>{word.text}</span>
+                                                </span>
+                                        ))}
+                                    </span>
                                 </span>
-                            </span>
-                        </h1>
-                        <h1>into Real Project</h1>
-                        <h1>that Deliver Results</h1>
+                            </h1>
+                            <h1>into Real Project</h1>
+                            <h1>that Deliver Results</h1>
+                        </div>
+
+                        <p>
+                            Hi, I am Kartavya Shrivastav, a developer based in India with a passion for building impactful digital experiences. I specialize in crafting modern web applications that blend creativity with functionality.
+                        </p>
+
+                        <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="See my Work" />
                     </div>
+                </header>
 
-                    <p>
-                        Hi, I am Kartavya Shrivastav, a developer based in India with a passion for building impactful digital experiences. I specialize in crafting modern web applications that blend creativity with functionality.
-                    </p>
+                {/*Right: Hero Model*/}
+                <figure>
+                    <div className="hero-3d-layout">
+                        <HeroExperience />
+                    </div>
+                </figure>
 
-                    <Button className="md:w-80 md:h-16 w-60 h-12" id="button" text="See my Work" />
-                </div>
-            </header>
             </div>
-
         </section>
     );
 };
