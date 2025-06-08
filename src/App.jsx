756a145c28a3248023c73b0f055c6c84cfefca5b
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-08 20:07:37 +0530

    Fix import and usage of LogoMarque component in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Hero from './sections/Hero.jsx';
 import ShowcaseSection from './sections/ShowcaseSection.jsx';
 import NavBar from './components/NavBar.jsx';
-import LogoShowcase from "./sections/LogoShowcase";
+import LogoMarque from "./sections/LogoMarque.jsx";
 import FeatureCards from "./sections/FeatureCards.jsx";
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
             <NavBar />
             <Hero />
             <ShowcaseSection />
-            <LogoShowcase />
+            <LogoMarque />
             <FeatureCards />
         </>
     );
