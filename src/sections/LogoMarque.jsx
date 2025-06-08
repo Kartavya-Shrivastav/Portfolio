3bf1dd0616b57e2e26c7465537e70c0fad37863b
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-08 20:07:48 +0530

    Add LogoMarque component to showcase logos in a marquee format

--- src/sections/LogoMarque.jsx
+++ src/sections/LogoMarque.jsx
@@ -10,7 +10,7 @@ const LogoIcon = ({ icon }) => {
 
 const LogoShowcase = () => (
   <div className="md:my-20 my-10 relative">
-    <div className="gradient-edge" />
+    <div className="gradient-edge" /> 
     <div className="gradient-edge" />
 
     <div className="marquee h-52">
