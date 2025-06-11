0b53f49b5ff3910e211d22abd55a5319a21f3592
Kartavya Shrivastav <kartavyashrivastav28@gmail.com>
2025-06-11 23:51:31 +0530

    Finished the ExperienceSection with GSAP animations for timeline cards and text elements

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -5,17 +5,63 @@ import gsap from 'gsap';
 import {useGSAP} from '@gsap/react';
 import {ScrollTrigger} from 'gsap/ScrollTrigger';
 
-gsap.resgiterPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
 const ExperienceSection = () => {
-  return (
+
+    useGSAP(() => {
+        gsap.utils.toArray('.timeline-card').forEach((card) => {
+            gsap.from(card, {
+                xPercent: -100,                     // start from the left
+                opacity: 0,                         // fade in    
+                duration: 1,                        // duration of the animation
+                ease: 'power2.inOut',               // easing function (starts slow, speeds up, then slows down)
+                transformOrigin:'left left',        // transform origin for the animation (left side of the card)
+                scrollTrigger: {                    // trigger the animation when the card is in view   
+                    trigger: card,
+                    start: 'top 80%',               // start the animation when the top of the card is 80% from the top of the viewport
+                },
+            });
+        });
+
+        gsap.to('.timeline', {
+            transformOrigin: 'bottom bottom', 
+            ease: 'power1.inOut',
+            scrollTrigger: {
+                trigger: '.timeline',
+                start: 'top center',
+                end: '70% center',
+                onUpdate: (self) => {
+                    gsap.to('.timeline', {
+                        scaleY: 1 - self.progress,
+                    });
+                },
+            },
+        });
+    
+        gsap.utils.toArray('.expText').forEach((text) => {
+            gsap.from(text, {
+                xPercent: 0,                     // start from the left
+                opacity: 0,                         // fade in    
+                duration: 1,                        // duration of the animation
+                ease: 'power2.inOut',               // easing function (starts slow, speeds up, then slows down)
+                scrollTrigger: {                    // trigger the animation when the card is in view   
+                    trigger: text,
+                    start: 'top 60%',               // start the animation when the top of the card is 80% from the top of the viewport
+                },
+            });
+        });
+
+    }, []);
+    
+    return (
     <section id="experience" className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
         <div className='w-full h-full md:px-20 px-5'>
             <TitleHeader title="Professional Work Experience" sub="💼 My Career Overview" />
             
             <div className='mt-32 relative'>
                 <div className='relative z-50 xl:space-y-32 space-y-10'>
-                    // experience cards
+                    {/* experience cards */}
                     {expCards.map((card) => (
                         <div key={card.title} className='exp-card-wrapper'>
                             <div className='xl:w-2/6'>
@@ -26,7 +72,7 @@ const ExperienceSection = () => {
                             </HighlightBorderCard>
                             </div>
 
-                            // experience timeline
+                            {/* experience timeline */}
                             <div className='xl:w-4/6'>
                                 <div className='flex items-start'>
                                     <div className='timeline-wrapper'>
@@ -34,21 +80,25 @@ const ExperienceSection = () => {
                                         <div className='gradient-line w-1 h-full' />
                                     </div>
 
-                                    <div>
-                                        <h1 className='font-semibold text-3xl'>{card.title}</h1>
-                                        <p className='my-5 text-white-50'>🗓️ {card.date}</p>
-                                        <p className='text-[#839cb5] italic'>Responsibilities</p>
-                                        <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
-                                            {card.responsibilities.map((responsibility) => (
-                                                <li key={responsibility} className='text-lg'>
-                                                    {responsibility}
-                                                </li>
-                                            ))}
-                                        </ul>
+                                    <div className='expText flex xl:gap-20 md:gap-10 gap-5 relative z-20'>
+                                        <div className='timeline-logo'>
+                                            <img src={card.logoPath} alt="logo" />
+                                        </div>
+                                        <div>
+                                            <h1 className='font-semibold text-3xl'>{card.title}</h1>
+                                            <p className='my-5 text-white-50'>🗓️ {card.date}</p>
+                                            <p className='text-[#839cb5] italic'>Responsibilities</p>
+                                            <ul className='list-disc ms-5 mt-5 flex flex-col gap-5 text-white-50'>
+                                                {card.responsibilities.map((responsibility) => (
+                                                    <li key={responsibility} className='text-lg'>
+                                                        {responsibility}
+                                                    </li>
+                                                ))}
+                                            </ul>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
-
                         </div> 
                     ))}
                 </div>
